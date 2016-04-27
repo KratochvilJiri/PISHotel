7ced636c873b4895f3abc887f0185479b7c04f40
Jiří Kratochvíl <juurakratochvil@gmail.com>
2016-04-27 17:48:20 +0200

    #33

--- public/js/controllers/LoginCtrl.js
+++ public/js/controllers/LoginCtrl.js
@@ -1,18 +1,24 @@
-angular.module('LoginCtrl', []).controller('LoginController', ['$scope','$state', '$location','AuthorizationService', function ($scope, $state, $location, AuthorizationService) {
+angular.module('LoginCtrl', []).controller('LoginController', ['$scope', '$state', '$location', 'AuthorizationService', function ($scope, $state, $location, AuthorizationService) {
 
     $scope.user = {};
 
     $scope.authorize = function () {
         AuthorizationService.authorize($scope.user)
-        .success(function (data) {
-            if(data.isValid){
-                $state.go('home.dashboard');
-            }
-            else{
-                // to do - error function
-                console.log("wrong pass/login - to do - print error function");
-            }
-            
-        });
+            .success(function (data) {
+                if (data.isValid) {
+                    $state.go('home.dashboard');
+                }
+                else {
+                    // to do - error function
+                    $scope.showError(["Neplatné přihlašovací údaje."]);
+                }
+
+            });
+    }
+
+    $scope.showError = function (errors) {
+        $scope.errors = errors;
+        $('html,body').animate({ scrollTop: 0 }, 'slow');
+        $('.message.status-alert').fadeIn();
     }
 }]);
